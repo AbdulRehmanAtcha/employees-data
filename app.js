ea6c15bf4226fdb0e999bb3bba559964c3c522a0
Abdul rehman <92687624+AbdulRehmanAtcha@users.noreply.github.com>
2022-09-15 15:30:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,365 @@
+var employeesData = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+    {
+        "id": 3,
+        "name": "Clementine Bauch",
+        "username": "Samantha",
+        "email": "Nathan@yesenia.net",
+        "address": {
+            "street": "Douglas Extension",
+            "suite": "Suite 847",
+            "city": "McKenziehaven",
+            "zipcode": "59590-4157",
+            "geo": {
+                "lat": "-68.6102",
+                "lng": "-47.0653"
+            }
+        },
+        "phone": "1-463-123-4447",
+        "website": "ramiro.info",
+        "company": {
+            "name": "Romaguera-Jacobson",
+            "catchPhrase": "Face to face bifurcated interface",
+            "bs": "e-enable strategic applications"
+        }
+    },
+    {
+        "id": 4,
+        "name": "Patricia Lebsack",
+        "username": "Karianne",
+        "email": "Julianne.OConner@kory.org",
+        "address": {
+            "street": "Hoeger Mall",
+            "suite": "Apt. 692",
+            "city": "South Elvis",
+            "zipcode": "53919-4257",
+            "geo": {
+                "lat": "29.4572",
+                "lng": "-164.2990"
+            }
+        },
+        "phone": "493-170-9623 x156",
+        "website": "kale.biz",
+        "company": {
+            "name": "Robel-Corkery",
+            "catchPhrase": "Multi-tiered zero tolerance productivity",
+            "bs": "transition cutting-edge web services"
+        }
+    },
+    {
+        "id": 5,
+        "name": "Chelsey Dietrich",
+        "username": "Kamren",
+        "email": "Lucio_Hettinger@annie.ca",
+        "address": {
+            "street": "Skiles Walks",
+            "suite": "Suite 351",
+            "city": "Roscoeview",
+            "zipcode": "33263",
+            "geo": {
+                "lat": "-31.8129",
+                "lng": "62.5342"
+            }
+        },
+        "phone": "(254)954-1289",
+        "website": "demarco.info",
+        "company": {
+            "name": "Keebler LLC",
+            "catchPhrase": "User-centric fault-tolerant solution",
+            "bs": "revolutionize end-to-end systems"
+        }
+    },
+    {
+        "id": 6,
+        "name": "Mrs. Dennis Schulist",
+        "username": "Leopoldo_Corkery",
+        "email": "Karley_Dach@jasper.info",
+        "address": {
+            "street": "Norberto Crossing",
+            "suite": "Apt. 950",
+            "city": "South Christy",
+            "zipcode": "23505-1337",
+            "geo": {
+                "lat": "-71.4197",
+                "lng": "71.7478"
+            }
+        },
+        "phone": "1-477-935-8478 x6430",
+        "website": "ola.org",
+        "company": {
+            "name": "Considine-Lockman",
+            "catchPhrase": "Synchronised bottom-line interface",
+            "bs": "e-enable innovative applications"
+        }
+    },
+    {
+        "id": 7,
+        "name": "Kurtis Weissnat",
+        "username": "Elwyn.Skiles",
+        "email": "Telly.Hoeger@billy.biz",
+        "address": {
+            "street": "Rex Trail",
+            "suite": "Suite 280",
+            "city": "Howemouth",
+            "zipcode": "58804-1099",
+            "geo": {
+                "lat": "24.8918",
+                "lng": "21.8984"
+            }
+        },
+        "phone": "210.067.6132",
+        "website": "elvis.io",
+        "company": {
+            "name": "Johns Group",
+            "catchPhrase": "Configurable multimedia task-force",
+            "bs": "generate enterprise e-tailers"
+        }
+    },
+    {
+        "id": 8,
+        "name": "Nicholas Runolfsdottir V",
+        "username": "Maxime_Nienow",
+        "email": "Sherwood@rosamond.me",
+        "address": {
+            "street": "Ellsworth Summit",
+            "suite": "Suite 729",
+            "city": "Aliyaview",
+            "zipcode": "45169",
+            "geo": {
+                "lat": "-14.3990",
+                "lng": "-120.7677"
+            }
+        },
+        "phone": "586.493.6943 x140",
+        "website": "jacynthe.com",
+        "company": {
+            "name": "Abernathy Group",
+            "catchPhrase": "Implemented secondary concept",
+            "bs": "e-enable extensible e-tailers"
+        }
+    },
+    {
+        "id": 9,
+        "name": "Glenna Reichert",
+        "username": "Delphine",
+        "email": "Chaim_McDermott@dana.io",
+        "address": {
+            "street": "Dayna Park",
+            "suite": "Suite 449",
+            "city": "Bartholomebury",
+            "zipcode": "76495-3109",
+            "geo": {
+                "lat": "24.6463",
+                "lng": "-168.8889"
+            }
+        },
+        "phone": "(775)976-6794 x41206",
+        "website": "conrad.com",
+        "company": {
+            "name": "Yost and Sons",
+            "catchPhrase": "Switchable contextually-based project",
+            "bs": "aggregate real-time technologies"
+        }
+    },
+    {
+        "id": 10,
+        "name": "Clementina DuBuque",
+        "username": "Moriah.Stanton",
+        "email": "Rey.Padberg@karina.biz",
+        "address": {
+            "street": "Kattie Turnpike",
+            "suite": "Suite 198",
+            "city": "Lebsackbury",
+            "zipcode": "31428-2261",
+            "geo": {
+                "lat": "-38.2386",
+                "lng": "57.2232"
+            }
+        },
+        "phone": "024-648-3804",
+        "website": "ambrose.net",
+        "company": {
+            "name": "Hoeger LLC",
+            "catchPhrase": "Centralized empowering task-force",
+            "bs": "target end-to-end models"
+        }
+    }
+]
+document.querySelector(".data").style.display = "none";
+document.querySelector(".employees").style.display = "flex";
+
+document.getElementById("emp1ID").innerText = employeesData[0].id;
+document.getElementById("emp1-name").innerText = employeesData[0].name;
+document.getElementById("emp2ID").innerText = employeesData[1].id;
+document.getElementById("emp2-name").innerText = employeesData[1].name;
+document.getElementById("emp3ID").innerText = employeesData[2].id;
+document.getElementById("emp3-name").innerText = employeesData[2].name;
+document.getElementById("emp4ID").innerText = employeesData[3].id;
+document.getElementById("emp4-name").innerText = employeesData[3].name;
+document.getElementById("emp5ID").innerText = employeesData[4].id;
+document.getElementById("emp5-name").innerText = employeesData[4].name;
+document.getElementById("emp6ID").innerText = employeesData[5].id;
+document.getElementById("emp6-name").innerText = employeesData[5].name;
+document.getElementById("emp7ID").innerText = employeesData[6].id;
+document.getElementById("emp7-name").innerText = employeesData[6].name;
+document.getElementById("emp8ID").innerText = employeesData[7].id;
+document.getElementById("emp8-name").innerText = employeesData[7].name;
+document.getElementById("emp9ID").innerText = employeesData[8].id;
+document.getElementById("emp9-name").innerText = employeesData[8].name;
+document.getElementById("emp10ID").innerText = employeesData[9].id;
+document.getElementById("emp10-name").innerText = employeesData[9].name;
+
+function showData(args) {
+    document.querySelector(".data").style.display = "block";
+    document.querySelector(".employees").style.display = "none";
+    if(args === "1"){
+        document.getElementById("emp-name").innerText = employeesData[0].name;
+        document.getElementById("emp-username").innerText = employeesData[0].username;
+        document.getElementById("emp-email").innerText = employeesData[0].email;
+        document.getElementById("emp-address").innerText = employeesData[0].address.street+" "+employeesData[0].address.suite+" "+employeesData[0].address.city;
+        document.getElementById("emp-zip").innerText = employeesData[0].address.zipcode;
+        document.getElementById("emp-number").innerText = employeesData[0].phone;
+        document.getElementById("emp-web").innerText = employeesData[0].website;
+        document.getElementById("emp-company").innerText = employeesData[0].company.name;
+    }
+    else if(args === "2"){
+        document.getElementById("emp-name").innerText = employeesData[1].name;
+        document.getElementById("emp-username").innerText = employeesData[1].username;
+        document.getElementById("emp-email").innerText = employeesData[1].email;
+        document.getElementById("emp-address").innerText = employeesData[1].address.street+" "+employeesData[1].address.suite+" "+employeesData[1].address.city;
+        document.getElementById("emp-zip").innerText = employeesData[1].address.zipcode;
+        document.getElementById("emp-number").innerText = employeesData[1].phone;
+        document.getElementById("emp-web").innerText = employeesData[1].website;
+        document.getElementById("emp-company").innerText = employeesData[1].company.name;
+    }
+    else if(args === "3"){
+        document.getElementById("emp-name").innerText = employeesData[2].name;
+        document.getElementById("emp-username").innerText = employeesData[2].username;
+        document.getElementById("emp-email").innerText = employeesData[2].email;
+        document.getElementById("emp-address").innerText = employeesData[2].address.street+" "+employeesData[2].address.suite+" "+employeesData[2].address.city;
+        document.getElementById("emp-zip").innerText = employeesData[2].address.zipcode;
+        document.getElementById("emp-number").innerText = employeesData[2].phone;
+        document.getElementById("emp-web").innerText = employeesData[2].website;
+        document.getElementById("emp-company").innerText = employeesData[2].company.name;
+    }
+    else if(args === "4"){
+        document.getElementById("emp-name").innerText = employeesData[3].name;
+        document.getElementById("emp-username").innerText = employeesData[3].username;
+        document.getElementById("emp-email").innerText = employeesData[3].email;
+        document.getElementById("emp-address").innerText = employeesData[3].address.street+" "+employeesData[3].address.suite+" "+employeesData[3].address.city;
+        document.getElementById("emp-zip").innerText = employeesData[3].address.zipcode;
+        document.getElementById("emp-number").innerText = employeesData[3].phone;
+        document.getElementById("emp-web").innerText = employeesData[3].website;
+        document.getElementById("emp-company").innerText = employeesData[3].company.name;
+    }
+    else if(args === "5"){
+        document.getElementById("emp-name").innerText = employeesData[4].name;
+        document.getElementById("emp-username").innerText = employeesData[4].username;
+        document.getElementById("emp-email").innerText = employeesData[4].email;
+        document.getElementById("emp-address").innerText = employeesData[4].address.street+" "+employeesData[4].address.suite+" "+employeesData[4].address.city;
+        document.getElementById("emp-zip").innerText = employeesData[4].address.zipcode;
+        document.getElementById("emp-number").innerText = employeesData[4].phone;
+        document.getElementById("emp-web").innerText = employeesData[4].website;
+        document.getElementById("emp-company").innerText = employeesData[4].company.name;
+    }
+    else if(args === "6"){
+        document.getElementById("emp-name").innerText = employeesData[5].name;
+        document.getElementById("emp-username").innerText = employeesData[5].username;
+        document.getElementById("emp-email").innerText = employeesData[5].email;
+        document.getElementById("emp-address").innerText = employeesData[5].address.street+" "+employeesData[5].address.suite+" "+employeesData[5].address.city;
+        document.getElementById("emp-zip").innerText = employeesData[5].address.zipcode;
+        document.getElementById("emp-number").innerText = employeesData[5].phone;
+        document.getElementById("emp-web").innerText = employeesData[5].website;
+        document.getElementById("emp-company").innerText = employeesData[5].company.name;
+    }
+    else if(args === "7"){
+        document.getElementById("emp-name").innerText = employeesData[6].name;
+        document.getElementById("emp-username").innerText = employeesData[6].username;
+        document.getElementById("emp-email").innerText = employeesData[6].email;
+        document.getElementById("emp-address").innerText = employeesData[6].address.street+" "+employeesData[6].address.suite+" "+employeesData[6].address.city;
+        document.getElementById("emp-zip").innerText = employeesData[6].address.zipcode;
+        document.getElementById("emp-number").innerText = employeesData[6].phone;
+        document.getElementById("emp-web").innerText = employeesData[6].website;
+        document.getElementById("emp-company").innerText = employeesData[6].company.name;
+    }
+    else if(args === "8"){
+        document.getElementById("emp-name").innerText = employeesData[7].name;
+        document.getElementById("emp-username").innerText = employeesData[7].username;
+        document.getElementById("emp-email").innerText = employeesData[7].email;
+        document.getElementById("emp-address").innerText = employeesData[7].address.street+" "+employeesData[7].address.suite+" "+employeesData[7].address.city;
+        document.getElementById("emp-zip").innerText = employeesData[7].address.zipcode;
+        document.getElementById("emp-number").innerText = employeesData[7].phone;
+        document.getElementById("emp-web").innerText = employeesData[7].website;
+        document.getElementById("emp-company").innerText = employeesData[7].company.name;
+    }
+    else if(args === "9"){
+        document.getElementById("emp-name").innerText = employeesData[8].name;
+        document.getElementById("emp-username").innerText = employeesData[8].username;
+        document.getElementById("emp-email").innerText = employeesData[8].email;
+        document.getElementById("emp-address").innerText = employeesData[8].address.street+" "+employeesData[8].address.suite+" "+employeesData[8].address.city;
+        document.getElementById("emp-zip").innerText = employeesData[8].address.zipcode;
+        document.getElementById("emp-number").innerText = employeesData[8].phone;
+        document.getElementById("emp-web").innerText = employeesData[8].website;
+        document.getElementById("emp-company").innerText = employeesData[8].company.name;
+    }
+    else if(args === "10"){
+        document.getElementById("emp-name").innerText = employeesData[9].name;
+        document.getElementById("emp-username").innerText = employeesData[9].username;
+        document.getElementById("emp-email").innerText = employeesData[9].email;
+        document.getElementById("emp-address").innerText = employeesData[9].address.street+" "+employeesData[9].address.suite+" "+employeesData[9].address.city;
+        document.getElementById("emp-zip").innerText = employeesData[9].address.zipcode;
+        document.getElementById("emp-number").innerText = employeesData[9].phone;
+        document.getElementById("emp-web").innerText = employeesData[9].website;
+        document.getElementById("emp-company").innerText = employeesData[9].company.name;
+    }
+}
+
+function hideData() {
+    document.querySelector(".data").style.display = "none";
+    document.querySelector(".employees").style.display = "flex";
+}
